c56a2a45ba2880174b1855798d581eb0cb8c1709
Karl Franke <karl.franke@outlook.de>
2022-07-27 12:34:46 +0200

    Update

---
+++ launch_cam.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Launch the robot
+#source /opt/ros/noetic/setup.bash 
+#source /home/user/catkin_ws/devel/setup.bash 
+
+echo "Launching Cam, please wait!"
+
+until roslaunch akrobat_cam Akrobat_Master_Mapping.launch
+do 
+    echo "Shutdown"
+    sleep 2
+    #sudo shutdown now
+done
+ 
