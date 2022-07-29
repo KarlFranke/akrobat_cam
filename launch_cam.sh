b647ff22c3740d651e3d22dd0f11a1508ed54a11
Karl Franke <karl.franke@outlook.de>
2022-07-29 13:25:49 +0200

    Update

--- launch_cam.sh
+++ launch_cam.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#https://risc.readthedocs.io/2-auto-service-start-afer-boot.html
+#https://askubuntu.com/questions/929659/bash-wait-for-a-ping-success
+
+source /opt/ros/kinetic/setup.bash
+source /home/odroid/catkin_ws/devel/setup.bash
+
 printf "%s" "waiting for ServerXY ..."
 while ! timeout 0.2 ping -c 1 -n 10.42.0.1 &> /dev/null
 do
