b770b0f867f4590550d4a297955aa1af760a6605
Karl Franke <karl.franke@outlook.de>
2022-08-02 09:08:37 +0200

    Update

--- launch_cam.sh
+++ launch_cam.sh
@@ -11,6 +11,10 @@ while ! timeout 0.2 ping -c 1 -n 10.42.0.1 &> /dev/null
 do
     printf "%c" "."
 done
+while ! rostopic list
+do
+    printf "%s" "Missing ROS_Master""
+done
 printf "\n%s\n"  "Server is back online"
 roslaunch akrobat_cam Akrobat_Master_Mapping.launch
  
